f6a8d7799adde698c5dcdaf098b0de3ca6cda08c
Alexander Krause <akr@informatik.uni-kiel.de>
2019-02-19 09:43:50 +0100

    use external DI binder and extend

--- src/main/java/net/explorviz/extension/dummy/main/DependencyInjectionBinder.java
+++ src/main/java/net/explorviz/extension/dummy/main/DependencyInjectionBinder.java
@@ -1,39 +1,18 @@
 package net.explorviz.extension.dummy.main;
 
-import com.github.jasminb.jsonapi.ResourceConverter;
-import javax.inject.Singleton;
-import net.explorviz.shared.common.jsonapi.ResourceConverterFactory;
-import net.explorviz.shared.config.annotations.ConfigValues;
-import net.explorviz.shared.config.annotations.injection.ConfigInjectionResolver;
-import net.explorviz.shared.config.annotations.injection.ConfigValuesInjectionResolver;
-import net.explorviz.shared.exceptions.ErrorObjectHelper;
-import org.glassfish.hk2.api.InjectionResolver;
-import org.glassfish.hk2.api.TypeLiteral;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import net.explorviz.shared.common.injection.CommonDependencyInjectionBinder;
 
 /**
  * The DependencyInjectionBinder is used to register Contexts and Dependency Injection (CDI) aspects
  * for this application.
  */
-public class DependencyInjectionBinder extends AbstractBinder {
+public class DependencyInjectionBinder extends CommonDependencyInjectionBinder {
 
   @Override
   public void configure() {
 
-    // Injectable config properties
-    this.bind(new ConfigInjectionResolver())
-        .to(new TypeLiteral<InjectionResolver<ConfigValues>>() {});
-    this.bind(new ConfigValuesInjectionResolver())
-        .to(new TypeLiteral<InjectionResolver<ConfigValues>>() {});
-
-    this.bindFactory(ResourceConverterFactory.class).to(ResourceConverter.class)
-        .in(Singleton.class);
-
-    // ErrorObject Handler
-    this.bind(ErrorObjectHelper.class).to(ErrorObjectHelper.class).in(Singleton.class);
-    
-    // TODO Common DI Binder
-
+    // Common DI
+    super.configure();
 
   }
 }
--- src/main/java/net/explorviz/extension/dummy/resources/TestResource.java
+++ src/main/java/net/explorviz/extension/dummy/resources/TestResource.java
@@ -1,6 +1,6 @@
 package net.explorviz.extension.dummy.resources;
 
-import javax.annotation.security.RolesAllowed;
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,8 +8,11 @@ import net.explorviz.extension.dummy.model.DummyModel;
 import net.explorviz.extension.dummy.model.SubDummyModel;
 
 @Path("test")
-@RolesAllowed({"admin"})
+// @RolesAllowed({"admin"})
+@PermitAll
 public class TestResource {
+
+  // Access annotations can also be applied at method level
 
   private static final String MEDIA_TYPE = "application/vnd.api+json";
 
